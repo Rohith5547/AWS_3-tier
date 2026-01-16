97a2d7c8aba304ed418b1450e8c36f9c8af7049c
Rohith5547 <thakur.rohith04@gmail.com>
2026-01-16 13:14:21 -0600

    still working on changes

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = ""
-    key            = ""
-    region         = var.region
-    encrypt        = true
-    dynamodb_table = "" # Name of your pre-created DynamoDB table
-  }
-}
