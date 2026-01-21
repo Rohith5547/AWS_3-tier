8d3b91e70a2ca1f6f9992cc83b52d942a790ae42
Rohith <thakur.rohith04@gmail>
2026-01-20 22:52:34 -0600

    saved file

--- terraform/modules/vpc/subnets.tf
+++ terraform/modules/vpc/subnets.tf
@@ -85,21 +85,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#create NAT gateway in public subnet of application vpc
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.example.id
-  subnet_id     = aws_subnet.public_subnets.id
-
-  tags = {
-    Name = "gw NAT"
-  }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw]
-}
-
-
 # 5. Add a route to the Internet Gateway in the public route table
 resource "aws_route" "igw_route" {
   route_table_id         = aws_route_table.public.id  
@@ -112,4 +97,59 @@ resource "aws_route_table_association" "public_subnet_association" {
   for_each       = aws_subnet.public_subnets
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
+}
+
+#NAT setting up
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public_subnets)[0].id
+
+  tags = {
+    Name = "${var.environment}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route_table" "nat" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.environment}-nat-rt"
+  }
+}
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.nat.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "nat_subnets" {
+  for_each = merge(
+    aws_subnet.app,
+    aws_subnet.ci_cd
+  )
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.nat.id
+}
+
+resource "aws_route_table" "isolated" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.environment}-isolated-rt"
+  }
+}
+
+resource "aws_route_table_association" "isolated_subnets" {
+  for_each = merge(
+    aws_subnet.web,
+    aws_subnet.db
+  )
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.isolated.id
 }
